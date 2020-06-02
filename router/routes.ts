79aa8da30b6d860629f07a8fecfc364431d7d0d7
Douglas Lima <doug.lb07@gmail.com>
2020-06-01 23:59:25 -0300

    feat: creating routes

---
+++ router/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'https://deno.land/x/oak/mod.ts'
+import beerController from './../controller/beerController.ts'
+
+const router: Router = new Router()
+
+router.get('/', beerController.index)
+router.get('/:id', beerController.show)
+router.post('/', beerController.store)
+router.put('/:id', beerController.update)
+router.delete('/:id', beerController.delete)
